fad6f3b8def04cf975468176493979ea979a0c45
andredarochasouza <andredarochasouza.12345@gmail.com>
2022-05-17 17:10:21 -0300

    att new trab 2 robolunar

---
+++ Trabalho 2 - POO - Robo Lunar/src/trabalho/Trabalho.java
@@ -0,0 +1,8 @@
+public class Trabalho {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        RoboLunar robo = new RoboLunar();
+    }
+}
