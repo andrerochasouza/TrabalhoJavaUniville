52710aa381ba55f1a287b9bd4701985ad19bdddd
André da Rocha Souza <58944953+andrerochasouza@users.noreply.github.com>
2022-09-21 20:21:11 -0300

    first commit by andre to work sistop

---
+++ Trabalho 1 - SISTOP - Trabalho 2/process.js
@@ -0,0 +1,27 @@
+class Process{
+    #nome;
+    #prioridade;
+    #carga;
+
+    constructor(nome, prioridade, carga) {
+        this.#name = nome;
+        this.#prioridade = prioridade;
+        this.#carga = carga;
+    }
+
+    getNome(){
+        return this.#nome;
+    }
+
+    getPrioridade(){
+        this.#prioridade;
+    }
+
+    getCarga(){
+        this.#carga;
+    }
+
+    setNome(){
+
+    }
+}
---
+++ Trabalho 1 - SISTOP - Trabalho 2/sketch.js
@@ -0,0 +1,17 @@
+var listProce = [];
+var 
+
+
+function fifo(listProcess){
+
+}
+
+function setup() {
+  createCanvas(720, 400);
+  background(0);
+  noSmooth();
+}
+
+function draw() {
+  // put drawing code here
+}
