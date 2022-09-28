9ef1bdebd526fec975915880d19b31c5373a3e3e
andre.rsouza <andredarochasouza.12345@gmail.com>
2022-09-28 01:20:56 -0300

    commit 2

--- Trabalho 1 - SISTOP - Trabalho 2/sketch.js
+++ Trabalho 1 - SISTOP - Trabalho 2/sketch.js
@@ -1,6 +1,43 @@
+class Process{
+  constructor(nome, prioridade, tempo) {
+      this.name = nome;
+      this.prioridade = prioridade;
+      this.tempo = tempo;
+  }
 
-var process = require('process');
+  getNome(){
+      return this.nome;
+  }
 
+  getPrioridade(){
+      return this.prioridade;
+  }
+
+  getTempo(){
+      return this.tempo;
+  }
+}
+
+const processos = [];
+const maxProcessos = 5;
+
+function createProcess(){
+  let nome = document.getElementById("nome").value;
+  let prioridade = document.getElementById("prioridade").value;
+  let tempo1 = document.getElementById("tempo1").value;
+  let tempo2 = document.getElementById("tempo2").value;
+  let tempo3 = document.getElementById("tempo3").value;
+  let tempo4 = document.getElementById("tempo4").value;
+  let tempo5 = document.getElementById("tempo5").value;
+
+  let tempo = [tempo1, tempo2, tempo3, tempo4, tempo5];
+
+  if(processos.length < maxProcessos){
+      processos.push(new Process(nome, prioridade, tempo));
+  }
+
+  console.log(processos);
+}
 
 
 function setup() {
@@ -9,10 +46,20 @@ function setup() {
   noLoop();
 }
 
+function fifo(){
+
+}
+
+function sjf(){
+
+}
+
+function prioridade(){
+
+}
+
 function draw() {
-
   
-
 }
 
 function mousePressed() {
