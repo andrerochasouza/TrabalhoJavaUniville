4eca8a2e8c261c8fe2f6b84a1673c962257a33e4
andre.rsouza <andredarochasouza.12345@gmail.com>
2022-09-28 01:47:21 -0300

    commit 3

--- Trabalho 1 - SISTOP - Trabalho 2/sketch.js
+++ Trabalho 1 - SISTOP - Trabalho 2/sketch.js
@@ -1,20 +1,20 @@
 class Process{
   constructor(nome, prioridade, tempo) {
-      this.name = nome;
-      this.prioridade = prioridade;
-      this.tempo = tempo;
+    this.nome = nome;
+    this.prioridade = prioridade;
+    this.tempo = tempo;
   }
 
   getNome(){
-      return this.nome;
+    return this.nome;
   }
 
   getPrioridade(){
-      return this.prioridade;
+    return this.prioridade;
   }
 
   getTempo(){
-      return this.tempo;
+    return this.tempo;
   }
 }
 
@@ -30,13 +30,56 @@ function createProcess(){
   let tempo4 = document.getElementById("tempo4").value;
   let tempo5 = document.getElementById("tempo5").value;
 
+  // verificar se o nome é vazio
+  if(nome == ""){
+    alert("Nome do processo não pode ser vazio");
+    return;
+  }
+
+  if (prioridade < 1 || prioridade > 5){
+    alert("Prioridade deve ser entre 1 a 5");
+    return;
+  }
+
+  for (let i = 0; i < 5; i++) {
+    if (tempo1 < -5 || tempo1 > 5) {
+      alert("O tempo deve estar entre -5 a 5");
+      return;
+    }
+  }
+
   let tempo = [tempo1, tempo2, tempo3, tempo4, tempo5];
 
   if(processos.length < maxProcessos){
-      processos.push(new Process(nome, prioridade, tempo));
+    processos.push(new Process(nome, prioridade, tempo));
   }
 
-  console.log(processos);
+  let processosDiv = document.getElementById("processos");
+  processosDiv.innerHTML = "";
+
+  for (let i = 0; i < processos.length; i++) {
+    let p = processos[i];
+    processosDiv.innerHTML += p.getNome() + " " + p.getPrioridade() + " " + p.getTempo() + " <button onclick='excluirProcesso(" + i + ")'>Excluir</button><br>";
+  }
+}
+
+function excluirProcesso(posicao){
+  processos.splice(posicao, 1);
+  let processosDiv = document.getElementById("processos");
+  processosDiv.innerHTML = "";
+
+  for (let i = 0; i < processos.length; i++) {
+    let p = processos[i];
+    processosDiv.innerHTML += p.getNome() + " " + p.getPrioridade() + " " + p.getTempo() + " <button onclick='excluirProcesso(" + i + ")'>Excluir</button><br>";
+  }
+}
+
+function temProcesso(){
+  if(processos.length > 0){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 
@@ -47,21 +90,29 @@ function setup() {
 }
 
 function fifo(){
+  if(temProcesso()){
 
+  } else {
+    alert("Não há processos para executar");
+  }
 }
 
 function sjf(){
+  if(temProcesso()){
 
+  } else {
+    alert("Não há processos para executar");
+  }
 }
 
 function prioridade(){
+  if(temProcesso()){
 
+  } else {
+    alert("Não há processos para executar");
+  }
 }
 
 function draw() {
-  
-}
 
-function mousePressed() {
-  redraw();
 }
