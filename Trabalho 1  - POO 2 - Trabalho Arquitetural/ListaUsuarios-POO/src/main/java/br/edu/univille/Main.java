4036613520e4d91f6293e447ff9eecfff56eb341
andre.rsouza <andredarochasouza.12345@gmail.com>
2022-10-14 19:42:50 -0300

    Preparação do projeto e construção do PropertiesUtils.

---
+++ Trabalho 1  - POO 2 - Trabalho Arquitetural/ListaUsuarios-POO/src/main/java/br/edu/univille/Main.java
@@ -0,0 +1,10 @@
+package br.edu.univille;
+
+import br.edu.univille.factory.ConnectionFactory;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+
+    }
+}
---
+++ Trabalho 1  - POO 2 - Trabalho Arquitetural/ListaUsuarios-POO/src/main/java/br/edu/univille/factory/ConnectionFactory.java
@@ -0,0 +1,6 @@
+package br.edu.univille.factory;
+
+public class ConnectionFactory {
+
+
+}
