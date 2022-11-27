17258c079029a45ba271ac39ebdfc65036ce85e5
andre.rsouza <andredarochasouza.12345@gmail.com>
2022-11-27 13:37:16 -0300

    Adição do JAR com BD e Swing

--- Trabalho final - POO 2 - Trabalho Final/ListaUsuarios-POO/src/main/java/br/edu/univille/factory/ConnectionFactory.java
+++ Trabalho final - POO 2 - Trabalho Final/ListaUsuarios-POO/src/main/java/br/edu/univille/factory/ConnectionFactory.java
@@ -1,23 +1,26 @@
 package br.edu.univille.factory;
 
+import javax.swing.*;
+import java.awt.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Objects;
 
-public class ConnectionFactory {
+public class ConnectionFactory extends javax.swing.JFrame {
 
 
     private static ConnectionFactory instance;
     private Connection connection;
-    private String url = PropertiesUtils.getUrlByProp();
 
-    private ConnectionFactory() throws SQLException {
+
+    private ConnectionFactory() {
         try {
             Class.forName("org.sqlite.JDBC");
-            this.connection = DriverManager.getConnection(url);
-        } catch (ClassNotFoundException e) {
-            System.out.println("Falha ao conectar ao banco de dados: " + e.getMessage());
+            this.connection = DriverManager.getConnection("jdbc:sqlite::resource:db.sqlite");
+            JOptionPane.showMessageDialog(null, "Conectado com sucesso!");
+        } catch (ClassNotFoundException | SQLException | HeadlessException e) {
+            JOptionPane.showMessageDialog(null, "Falha ao conectar ao banco de dados: " + e.getMessage());
         }
     }
 
